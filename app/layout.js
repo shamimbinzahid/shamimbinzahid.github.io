678d7af11a2fbf933aa13a8d97c4602af912990c
shamemezahid <2017-815-006@student.cse.du.ac.bd>
2025-09-16 01:20:21 +0600

    gtag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Red_Hat_Display, Noto_Sans_Mono } from 'next/font/google';
 import "./globals.css";
+import Script from 'next/script';
 
 const displayFont = Red_Hat_Display({
   subsets: ['latin'],
@@ -16,15 +17,38 @@ export const metadata = {
   description: "Product, Usability, Engineering, Design",
 };
 
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        async
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-9RRRYEHJBK"}
+      />
+      <Script id="ga-id" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9RRRYEHJBK');
+        `}
+      </Script>
+    </>
+  );
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${monoFont.variable} ${displayFont.variable}`}>
-      <link
+      <head>
+        <link
           rel="icon"
           href="shamim.ico"
           type="image/jpeg"
           sizes="32x32"
         />
+        <GoogleAnalytics />
+      </head>
       <body className={`${monoFont.className}`}>
         {children}
       </body>
